7cb1821e34d0a0e938b9f2c1122402883efaac88
Lucky4rever <snigur.pavlo@lll.kpi.ua>
2023-10-03 18:47:58 +0300

    Add project files.

---
+++ Web.Net 23-24/Lab1/CustomArray.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CustomArray<T> : IEnumerable<T>
+{
+    private T[] _array;
+    private int _startIndex;
+
+    public CustomArray(int size, int startIndex)
+    {
+        if (size <= 0)
+        {
+            throw new ArgumentException("You should set size bigger than 0");
+        }
+
+        this._startIndex = startIndex;
+        this._array = new T[size];
+    }
+
+    public CustomArray(T[] initialArray, int startIndex)
+    {
+        if (initialArray == null)
+        {
+            throw new ArgumentNullException("Selected nullable data");
+        }
+
+        this._startIndex = startIndex;
+        this._array = initialArray;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (!IsIndexValid(index))
+            {
+                throw new IndexOutOfRangeException("Out of range");
+            }
+
+            return this._array[index - this._startIndex];
+        }
+        set
+        {
+            if (!IsIndexValid(index))
+            {
+                throw new IndexOutOfRangeException("Out of range");
+            }
+
+            this._array[index - this._startIndex] = value;
+        }
+    }
+
+    public int StartIndex => this._startIndex;
+    public int Length => this._array.Length;
+    public T[] Array => this._array;
+    public int FirstIndex => this._startIndex;
+    public int LastIndex => this._startIndex + this._array.Length - 1;
+
+    public T[] ToStandardArray()
+    {
+        return this._array;
+    }
+
+    private bool IsIndexValid(int index)
+    {
+        return index >= this._startIndex && index < this._startIndex + this._array.Length;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this._array.Length; i++)
+        {
+            yield return this._array[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Web.Net 23-24/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CustomArray<int> array = new CustomArray<int>(new int[] { 1, 3, 2 }, 4);
+        Console.WriteLine($"Item is {array[5]}");
+
+        Console.ReadKey();
+    }
+}
